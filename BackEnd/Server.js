13e6649af21b79740be6d3cdb05077316b53f392
AhmadKhan9 <mafriand77@gmail.com>
2024-05-14 12:18:44 +0500

    latest

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,17 +1,25 @@
 const dotenv = require('dotenv')
 dotenv.config({path : './config.env'})
+const os = require('os')
+const cluster = require('cluster')
 const mongoose = require('mongoose')
 
 
 const DB = process.env.DB.replace('<PASSWORD>' , process.env.DB_PASSWORD) 
 mongoose.connect(DB).then(()=> console.log('database connected'))
 
+if(cluster.isPrimary){
 
+    for(let i = 0 ; i < os.cpus().length ; i++){
+        cluster.fork()
+    }
 
-const app = require('./App')
-
-const Port = process.env.PORT || 3000
-console.log(Port)
-app.listen(Port , ()=> {
-    console.log('server is running')
-})
+}else{
+    const app = require('./App')
+    
+    const Port = process.env.PORT || 3000
+    console.log(Port)
+    app.listen(Port , ()=> {
+        console.log('server is running')
+    })
+}
